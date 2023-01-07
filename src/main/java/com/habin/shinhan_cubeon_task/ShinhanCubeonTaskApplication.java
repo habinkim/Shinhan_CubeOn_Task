fa54fb5f73004219c9451777bc2d071fca146be8
habin <danny9643@naver.com>
2023-01-07 16:34:54 +0900

    first commit

---
+++ src/main/java/com/habin/shinhan_cubeon_task/ShinhanCubeonTaskApplication.java
@@ -0,0 +1,13 @@
+package com.habin.shinhan_cubeon_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinhanCubeonTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShinhanCubeonTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/habin/shinhan_cubeon_task/ShinhanCubeonTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habin.shinhan_cubeon_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinhanCubeonTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
