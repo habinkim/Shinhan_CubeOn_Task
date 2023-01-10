b5a36695ab82b36b784e85ebbef5d5c6a3870aa6
habin <danny9643@naver.com>
2023-01-10 16:31:01 +0900

    p6spy Config 추가

---
+++ src/main/java/com/habin/shinhan_cubeon_task/common/config/P6spyLogMessageFormatConfig.java
@@ -0,0 +1,16 @@
+package com.habin.shinhan_cubeon_task.common.config;
+
+import com.p6spy.engine.spy.P6SpyOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class P6spyLogMessageFormatConfig {
+
+    @PostConstruct
+    public void setLogMessageFormat() {
+        P6SpyOptions.getActiveInstance().setLogMessageFormat(P6spySqlFormatConfig.class.getName());
+    }
+
+}
